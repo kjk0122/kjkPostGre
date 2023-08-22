52cffa3b243c8717f4ffaaa8b769c77211d2ab48
kjk0122 <juke9548@gmail.com>
2023-08-22 19:43:18 +0900

    중간 세이브

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.LoginRequestDto;
+import com.example.demo.dto.SignupRequestDto;
+import com.example.demo.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    @GetMapping("/signup")
+    public ModelAndView signupPage() {
+        return new ModelAndView("signup");
+    }
+    @GetMapping("/login")
+    public ModelAndView loginPage() {
+        return new ModelAndView("login");
+    }
+    @PostMapping("/signup")
+    @ApiOperation(value = "회원가입")
+    public ResponseEntity<?> signup(@RequestBody SignupRequestDto signupRequestDto) {
+        userService.signup(signupRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                             .body("회원가입이 완료되었습니다.");
+    }
+    @PostMapping("/login")
+    @ApiOperation(value = "로그인")
+    @ResponseBody
+    public String login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        userService.login(loginRequestDto, response);
+        return "success";
+    }
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,65 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.LoginRequestDto;
+import com.example.demo.dto.SignupRequestDto;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    String pt = "^[a-z\\\\d`~!@#$%^&*()-_=+]{4,10}$"; String ptt = "^[a-zA-Z\\\\d`~!@#$%^&*()-_=+]{8,15}$";
+
+    @Transactional
+    public User signup(SignupRequestDto signupRequestDto) {
+        //이름, 비밀번호 대조를 위해 값을 뽑아놓음
+        String username = signupRequestDto.getUsername();
+        String pwcheck =  signupRequestDto.getPassword();
+
+        //username 확인
+        if(!Pattern.matches(pt, username)){
+            throw new IllegalArgumentException(
+                    "아이디는 최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9)로 되어야합니다.");
+        }
+        //비밀번호 확인
+        if(!Pattern.matches(ptt, pwcheck)){
+            throw new IllegalArgumentException(
+                    "비밀번호는 최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9), 특수문자로 되어야합니다.");
+        }
+        // 회원 중복 확인
+        Optional<User> found = userRepository.findByUsername(username);
+        if (found.isPresent()) {
+            throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+        }
+        //저장을 바로 하면 안되고 encoding해서 저장하기
+        String password = passwordEncoder.encode(pwcheck);
+        //등록등록
+        User user = new User(username, password);
+        User savedUser = userRepository.save(user);
+        return savedUser;
+    }
+    @Transactional(readOnly = true)
+    public void login(LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        //이름, 비밀번호 대조를 위해 값을 뽑아놓음
+        String username = loginRequestDto.getUsername();
+        String password = loginRequestDto.getPassword();
+        // 사용자 확인
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new IllegalArgumentException("등록된 사용자가 없습니다.")
+        );
+        // 비밀번호 확인
+        if(!passwordEncoder.matches(password, user.getPassword())){
+            throw  new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+    }
+}
